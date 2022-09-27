1488692d15ef1513109348d3079d9f17f980c7ad
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-09-27 15:26:53 +0300

    refactor Portfolio and services pages

--- src/pages/work-services/Services.tsx
+++ src/pages/work-services/Services.tsx
@@ -8,10 +8,23 @@ import {
   PageTitle,
   PageTitleContainer,
   Route,
+  ImageWrapper,
+  ImageContainer,
+  Image,
 } from '../../components/common/Common.styles'
 import Header from '../../components/header/Header'
 import { PageRouteContainer } from '../contact/Contact.styles'
 import { useNavigate } from 'react-router-dom'
+import BG1 from '../../assets/photog1.jpg'
+import BG2 from '../../assets/photog2.jpg'
+import BG3 from '../../assets/photog3.jpg'
+import BG4 from '../../assets/photog4.jpg'
+import PG1 from '../../assets/pg1.jpg'
+import PG2 from '../../assets/pg2.jpg'
+import PG3 from '../../assets/pg3.jpg'
+import PG4 from '../../assets/pg4.jpg'
+import PG5 from '../../assets/pg5.jpg'
+import PG6 from '../../assets/pg6.jpg'
 
 const Services = () => {
   const navigate = useNavigate()
@@ -38,7 +51,40 @@ const Services = () => {
             photography services I provide.
           </PageDescription>
         </PageDescriptionContainer>
-        <PageBody>hello</PageBody>
+        <PageBody>
+          <ImageWrapper>
+            <ImageContainer>
+              <Image src={BG1} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={BG2} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={BG3} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={BG4} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={PG1} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={PG2} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={PG3} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={PG4} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={PG5} />
+            </ImageContainer>
+            <ImageContainer>
+              <Image src={PG6} />
+            </ImageContainer>
+          </ImageWrapper>
+        </PageBody>
       </PageContent>
     </Container>
   )
